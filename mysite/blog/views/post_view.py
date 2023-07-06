86fe8c3731cba7028715ccb912ff680ba999c2e3
edumarques7 <marques.barbosa@hotmail.com>
2023-07-06 18:40:17 -0300

    django template exercicio

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -7,6 +7,7 @@ class PostView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
 
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = "post_detail.html"
