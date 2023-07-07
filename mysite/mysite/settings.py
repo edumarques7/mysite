59d34a33c4cf2e6ddcb5a6001b2a8aab5ec0d842
edumarques7 <marques.barbosa@hotmail.com>
2023-07-07 11:40:54 -0300

    django forms exercicio

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
